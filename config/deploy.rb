c2e06bf0839759bdda9dad49f1e6874d877c6bff
urrik98 <adamrenschen@gmail.com>
2019-01-10 14:19:09 -0800

    ...

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,6 @@ set :console_user, :deployment
 # set :log_level,     :debug
 set :keep_releases, 5
 set :linked_filers, %w(config/master.key)
-
 ## Linked Files & Directories (Default None):
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
